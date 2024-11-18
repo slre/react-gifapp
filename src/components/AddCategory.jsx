bc26f619ffbe138fee337edb22e8936450583624
Salvador <isc.slre@gmail.com>
2024-11-18 13:22:23 -0600

    change props on AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const AddCategory = ( {setCategories} ) => {
+const AddCategory = ( {onNewCategory} ) => {
 
     const [inputValue, setInputValue] = useState('');
     const onInputChange = (event)=>{
@@ -9,7 +9,8 @@ const AddCategory = ( {setCategories} ) => {
     const onSubmit = (event)=>{
         event.preventDefault();
         if(inputValue.trim().length <=1) return;
-        setCategories(categories => [inputValue,...categories]);
+        //setCategories(categories => [inputValue,...categories]);
+        onNewCategory(inputValue.trim());
         setInputValue('');
     }    
     return (
