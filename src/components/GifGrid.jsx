92220a0dd133ff294a1f80dc9c44a1fd80d7448e
Salvador <isc.slre@gmail.com>
2024-11-25 10:42:08 -0600

    customhooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs';
 import { ImageItem } from './ImageItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 const GifGrid  = ({category}) => {
   
-    const [images, setImages] = useState([]);
-
-    const getImages = async()=>{
-        const newImages = await getGifs( category );
-        setImages(newImages);
-    }
-    useEffect(() => {
-        getImages();
-    }, [])
+   
     
-    //console.log(images);
+    const { images, isLoading  } = useFetchGifs(category);
     return (
-    <>
-      <h5>{category}</h5>
-      <div className='card-grid'>
-        {images.map((img) => (
-        <ImageItem 
-            key={img.id} 
-            //title={img.title} 
-            //url={img.url.url}
-            { ...img}  
-        />
-      ))}
-      </div>
+        <>
+            <h1>{category}</h1>
+            {
+                // isLoading === true
+                // ? ( <h2>Loading...</h2> )
+                // : null
+
+                isLoading && ( <h2>Loading...</h2> )
+            }
+            <h2></h2>
+            <div className='card-grid'>
+                {images.map((img) => (
+                <ImageItem 
+                    key={img.id} 
+                    //title={img.title} 
+                    //url={img.url.url}
+                    { ...img}  
+                />
+            ))}
+            </div>
       
-    </>
+        </>
   );
 }
 
